04c28c1a19151731275b1067a4cc8d595acaa54c
fellowbeard <virgovibe@gmail.com>
2022-08-17 08:17:57 -1000

    adds IndexView

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import IndexView from "../views/IndexView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/recipes",
+    name: "index",
+    component: IndexView,
   },
   {
     path: "/about",
